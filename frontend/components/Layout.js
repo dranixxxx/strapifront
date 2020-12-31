6362b44ee0d94eba228a0a86ef3a8181b432bab8
dranixxxx <phoang0302@gmail.com>
2020-12-31 14:27:25 +0700

    first

---
+++ frontend/components/Layout.js
@@ -0,0 +1,107 @@
+/* /components/Layout.js */
+
+import React, { useContext } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Container, Nav, NavItem } from "reactstrap";
+import { logout } from "../lib/auth";
+import AppContext from "../context/AppContext";
+import Footer from "../components/Footer";
+import Dropdown from "../components/Dropdown";
+
+const Layout = (props) => {
+    const title = "Welcome to Nextjs";
+    const { user, setUser } = useContext(AppContext);
+
+    return (
+        <div>
+        <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossOrigin="anonymous"
+        />
+        <script src="https://js.stripe.com/v3" />
+        </Head>
+        <header>
+        <style jsx>
+    {`
+            a {
+              color: red;
+            }
+            h5 {
+              color: red;
+              padding-top: 11px;
+            }
+          `}
+</style>
+    <Nav className="navbar navbar-light " style={{borderBottom:"1px solid"}}>
+        <NavItem>
+        <Link href="/">
+        <a className="navbar-brand">EDUNET</a>
+        </Link>
+        </NavItem>
+        <NavItem ><Dropdown/></NavItem>
+        <NavItem>
+
+        <Link href="/test">
+        <a className="navbar-brand">Plan</a>
+    </Link>
+        <Link href="/MainPage">
+        <a className="navbar-brand">Contact sale</a>
+    </Link>
+        </NavItem>
+
+
+        <NavItem className="ml-auto">
+        </NavItem>
+        <NavItem>
+        {user ? (
+                //<h5>{user.username}</h5>
+            <Link href="https://localhost:3001">
+        <a className="nav-link"> {user.username}</a>
+    </Link>
+) : (
+    <Link href="/register">
+        <a className="nav-link"> Sign up</a>
+    </Link>
+)}
+
+</NavItem>
+
+    <NavItem>
+    {user ? (
+            <Link href="/">
+            <a
+        className="nav-link"
+        onClick={() => {
+        logout();
+        setUser(null);
+    }}
+>
+    Logout
+    </a>
+    </Link>
+) : (
+    <Link href="/login">
+        <a className="nav-link">Sign in</a>
+        </Link>
+)}
+</NavItem>
+    </Nav>
+    <div style={{backgroundColor:"#E4F7EB"}}>
+        <Container><p style={{color:"#1C7E41"}}>special offer</p></Container>
+        <br/>
+    </div>
+    </header>
+    <body>{props.children}</body>
+    <footer><Footer/></footer>
+    </div>
+);
+};
+
+export default Layout;
---
+++ frontend/components/account/Student.js
@@ -0,0 +1,118 @@
+import { useContext } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import AppContext from "../../context/AppContext";
+
+import {
+    Container,
+    Button,
+    Card,
+    CardBody,
+    CardText,
+    CardTitle,
+    Col,
+    Row,
+} from "reactstrap";
+import Link from "next/link";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
+const GET_TK = gql`
+    {
+    role (id: 2){
+        id
+        name
+        roleprices{
+          id
+          name
+          name_des
+          description
+          price
+        }
+    }
+  }
+`;
+
+function Student(props){
+    const appContext = useContext(AppContext);
+    const { loading, error, data } = useQuery(GET_TK);
+    if (error) return "Error loading";
+    if (loading) return <h1>Fetching</h1>;
+    return(
+        <>
+
+            <Container>
+
+            <h1>{data.role.name}</h1>
+            <Row>
+                <Col xs="6" sm="3" style={{ padding: 0 }} >
+            <Card style={{ height: "350px" }}>
+        <CardBody>
+            <CardTitle><h6>BASIC</h6></CardTitle>
+            <CardText><h6>Personal Meeting</h6></CardText>
+            <CardText><h3>Free</h3></CardText>
+            <div>
+            <a href="/register">
+            <Button
+            outline
+            color="primary"
+
+            >
+        Sign up now!
+        </Button>
+        </a>
+
+        </div>
+            <CardText>
+                <li>Host up to 100 participants</li>
+                <li>Group meetings for up to 40 minutes</li>
+                <li>Unlimited one-on-one Meetings</li>
+                <style jsx>
+                    {`
+                    li{
+                        font-size: 12.5px;
+                    `}
+                </style>
+            </CardText>
+
+        </CardBody>
+
+
+        </Card>
+        </Col>
+
+            {data.role.roleprices.map((res) => (
+                    <Col xs="6" sm="3" style={{ padding: 0 }} key={res.id}>
+            <Card style={{ height: "350px" }}>
+
+        <CardBody>
+            <CardTitle><h6>{res.name}</h6></CardTitle>
+            <CardText><h6>{res.name_des}</h6></CardText>
+            <CardText><h3>{res.price}</h3></CardText>
+            <div>
+            <Link
+                as={`/roleprice/${res.id}`}
+                href={`/roleprice?id=${res.id}`}
+            >
+                <a className="btn btn-primary">View</a>
+            </Link>
+
+        </div>
+            <CardText><li>{res.description}</li></CardText>
+
+        </CardBody>
+        </Card>
+        </Col>
+    ))}
+
+        </Row>
+        <br/>
+        </Container>
+
+        </>
+
+    );
+
+
+}
+
+    export default Student;
